2b66a2712525036629f9c603cd7aacf555995e4d
amona20 <amona20cr@gmail.com>
2025-10-25 19:09:29 +0200

    Početne postavke i komponenta Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React, { use, useState } from "react";
+import { NavLink } from "react-router-dom"
+import './Header.css'
+
+export default function Header () {
+    const [open, setOpen] = useState(false);
+
+    return (
+        <header className="header">
+            <div className="container header-inner">
+                <div className="logo">Turistička agencija</div>
+                <button type="button" 
+                className="travel-logo"
+                onClick={() => setOpen (!open)}>
+                     ✈︎
+                </button>
+                <nav className={`nav ${open ? "open" : ""}`}>
+                    <NavLink to="/" className={({isActive})=> {isActive ? "active" : ""}}>Naslovnica</NavLink>
+                    <NavLink to="/about"className={({isActive})=> {isActive ? "active" : ""}}>O nama</NavLink>
+                    <NavLink to="/contacts"className={({isActive})=> {isActive ? "active" : ""}}>Kontakt</NavLink>
+                    <NavLink to="/categories"className={({isActive})=> {isActive ? "active" : ""}}>Kategorije</NavLink>
+                    <NavLink to="/tours"className={({isActive})=> {isActive ? "active" : ""}}>Izleti</NavLink>
+                    <NavLink to="/tour-details"className={({isActive})=> {isActive ? "active" : ""}}>Detalji izleta</NavLink>
+                    <NavLink to="/fqa"className={({isActive})=> {isActive ? "active" : ""}}>F.Q.A</NavLink>
+                </nav>
+            </div>
+        </header>
+    )
+}
