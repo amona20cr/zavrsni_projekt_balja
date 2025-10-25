607bdf34eb767024afc972e20a0bab0d035aa4f9
amona20 <amona20cr@gmail.com>
2025-10-25 23:09:44 +0200

    Dio Tours jsx ispravci

--- src/pages/Tours.jsx
+++ src/pages/Tours.jsx
@@ -3,13 +3,17 @@ import { tours } from "../data/tours";
 
 
 export default function Tours () {
-    const categories = [...new Set(tours.map(c => c.category))];    
+    const categories = [...new Set(tours.map(c => c.category))]; 
+    const levels = [...new Set(tours.map(c => c.level))];   
+
     const [query, setQuery] = useState("");
     const [category, setCategory] = useState("Svi");
+    const [level, setLevel] = useState("Svi");
+    
 return (
     <section className="tours container">
         <h1>Putovanja</h1>
-        <div className="filters">
+        <div className="filters"> 
             <input 
              type="text"
              className="search"
@@ -19,13 +23,23 @@ return (
              />
              <div className="subfilters">
                 <select value={category} onChange={(e)=> setCategory(e.target.value) }>
-                    {categories.map((c) => (
+                   <option value="Svi">Svi</option>
+                   {categories.map((c) => (
                         <option key={c} value={c}>{c}</option>
                     ))}
+                </select>
+
+               <select value={level} onChange={(e) => setLevel(e.target.value)}>
+                    <option value="Svi">Svi</option>
+            {levels.map((lvl) => (
+                 <option key={lvl} value={lvl}>
+                 {lvl}
+             </option>
+                 ))}
                 </select>
              </div>
         </div>
     </section>
-    )
+    );
 }
 
